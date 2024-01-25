e73ccd56224bcb9b597f35790bdaaa52284fb1cb
iuliia <julia.malyh@gmail.com>
2024-01-25 13:31:32 +0100

    change1

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava;
 
 /**
  * @see <a href="http://topjava.herokuapp.com">Demo application</a>
+ * something change
  * @see <a href="https://github.com/JavaOPs/topjava">Initial project</a>
  */
 public class Main {
